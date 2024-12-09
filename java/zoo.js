0a8aac1c3d01a44f680c3585a65bce413f81c41b
Nandaleeb <noshiandaleeb@gmail.com>
2024-12-09 10:36:02 -0500

    Add files via upload

---
+++ java/zoo.js
@@ -0,0 +1,31 @@
+
+
+document.getElementById("top-ft").onclick = function() {infoMyFunction()};
+
+function infoMyFunction() {
+  document.getElementById("top-ft").textContent = "plz for more info exmple@exmple.com";
+}
+
+document.getElementById("button-s").onclick = function() {myFunction()};
+
+function myFunction() {
+  document.getElementById("button-s").textContent = "special event by appoinment only!";
+}
+
+// visiting hr
+document.getElementById("visiting-hr").onclick = function() {visitMyFunction()};
+
+function visitMyFunction() {
+  document.getElementById("visiting-hr").textContent = '8:00am-8:00pm';
+  
+}
+
+document.getElementById("guide").onclick = function() {guideMyFunction()};
+
+function guideMyFunction() {
+  document.getElementById("guide").textContent = 'moday Closed';
+  
+}
+
+
+
